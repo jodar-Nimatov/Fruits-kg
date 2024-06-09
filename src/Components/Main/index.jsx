d679770c80940e83d9db87963b6653db4d904f08
rakyp007 <tashbaev07rakyp@gmail.com>
2024-06-10 04:11:28 +0600

    main ready

--- src/Components/Main/index.jsx
+++ src/Components/Main/index.jsx
@@ -1,27 +1,37 @@
 import React from "react";
 import Header from "../Header/header";
 import { Link } from "react-router-dom";
-import Mainlogo from "../../assets/icons/mainlogo.svg"
-import "./main.scss"
+import Mainlogo from "../../assets/icons/mainlogo.svg";
+import "./main.scss";
 
 const index = () => {
   return (
     <>
-    <div className="main-back">
-    <div className="main-page">
-      <div className="container">
-        <Header />
-        <button><Link to="/menu">Каталог </Link></button>
-        <div className="main-logo-container">
-          <div className="main-logo">
-          <img src={Mainlogo} alt="" />
+      <div className="main-back">
+        <div className="main-page">
+          <div className="container">
+            <Header />
+            <div className="block-content">
+              <div className="main-logo-container">
+                <div className="main-logo">
+                  <img src={Mainlogo} alt="" />
+                </div>
+              </div>
+              <div className="main-text">
+                <h1>
+                  Насладитесь свежестью и вкусом наших фруктов и овощей,
+                  приготовленных с любовью и заботой о вашем здоровье!
+                </h1>
+              </div>
+              <div className="main-button">
+                <Link to="/menu">
+                  <button>Каталог </button>
+                </Link>
+              </div>
+            </div>
           </div>
         </div>
-        <div className="main-left"></div>
-        <div className="main-right"></div>
       </div>
-    </div>
-    </div>
     </>
   );
 };
