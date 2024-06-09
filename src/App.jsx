b6f86649203e8efa634d26483045c1a1272eec10
rakyp007 <tashbaev07rakyp@gmail.com>
2024-06-09 21:37:41 +0600

    app css

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,22 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import Menu from "./Components/Menu";
+import Main from "./Components/Main";
+import "./App.scss";
+
 function App() {
   return (
-    <div >
-      
-    </div>
+    <Router>
+      <div>
+        <nav>
+          
+        </nav>
+        <Routes>
+          <Route exact path="/" element={<Main />} />
+          <Route path="/menu" element={<Menu />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
--- src/Components/Main/index.jsx
+++ src/Components/Main/index.jsx
@@ -1,18 +1,18 @@
 import React from "react";
 import Header from "../Header/header";
+import { Link } from "react-router-dom";
 
 const index = () => {
   return (
-    <div>
       <div className="main-menu">
         <div className="container">
           <Header />
+          <Link to="/menu">Каталог </Link>
           <div className="main-logo"></div>
           <div className="main-left"></div>
           <div className="main-right"></div>
         </div>
       </div>
-    </div>
   );
 };
 
